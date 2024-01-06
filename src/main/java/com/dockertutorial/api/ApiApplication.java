b9f844b9dd03d85b04a177c04bd1a34bd7d1587c
Robertas <robertasvilkisius@gmail.com>
2024-01-06 19:09:26 +0200

    AOP and Mongo added

--- src/main/java/com/dockertutorial/api/ApiApplication.java
+++ src/main/java/com/dockertutorial/api/ApiApplication.java
@@ -2,8 +2,12 @@ package com.dockertutorial.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories
+@EnableAspectJAutoProxy(proxyTargetClass = true)
 public class ApiApplication {
 
 	public static void main(String[] args) {
