a19a782bd671d393a6d8bd70bf189e5a29643180
Robertas <robertasvilkisius@gmail.com>
2024-01-07 17:57:53 +0200

    test 2

--- src/main/java/com/dockertutorial/api/model/exception/LogError.java
+++ src/main/java/com/dockertutorial/api/model/exception/LogError.java
@@ -2,5 +2,5 @@ package com.dockertutorial.api.model.exception;
 
 public class LogError {
 
-    String message = "This is a test change";
+    String message = "This is a test change. This is a test change v2";
 }
